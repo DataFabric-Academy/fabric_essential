42d60744ecafa873a39cb9f71330c05930bccde0
Phakkhaphong Kritwat (Data & AI Architect) <phakkhaphong@msn.com>
2026-04-22 15:08:08 +0700

    Update 02_Load Data.sql

--- Demo/02_Load Data.sql
+++ Demo/02_Load Data.sql
@@ -1,71 +1,41 @@
 /*
-    COPY INTO Document
-    https://learn.microsoft.com/en-us/sql/t-sql/statements/copy-into-transact-sql?view=fabric&preserve-view=true
+    Corrected Script for Microsoft Fabric / Synapse
+    Separating Sample Dimensions (CSV) from Open Dataset Trips (Parquet)
 */
 
-COPY INTO [dbo].[Date]
-FROM 'https://azureopendatasets.blob.core.windows.net/nyctlc/yellow/puYear=2013/Date'
-WITH
-(
-    FILE_TYPE = 'CSV',
-    FIELDTERMINATOR = ',',
-    FIELDQUOTE = ''
-);
+-- ==========================================================
+-- 1. Dimension Tables (From Sample Data Endpoint - CSV)
+-- ==========================================================
 
+COPY INTO [dbo].[Date]
+FROM 'https://azuresynapsestorage.blob.core.windows.net/sampledata/nyctlc/date'
+WITH (FILE_TYPE = 'CSV', FIELDTERMINATOR = ',', FIELDQUOTE = '', FIRSTROW = 2);
 
 COPY INTO [dbo].[Geography]
-FROM 'https://azureopendatasets.blob.core.windows.net/nyctlc/yellow/puYear=2013/Geography'
-WITH
-(
-    FILE_TYPE = 'CSV',
-    FIELDTERMINATOR = ',',
-    FIELDQUOTE = ''
-);
+FROM 'https://azuresynapsestorage.blob.core.windows.net/sampledata/nyctlc/geography'
+WITH (FILE_TYPE = 'CSV', FIELDTERMINATOR = ',', FIELDQUOTE = '', FIRSTROW = 2);
 
 COPY INTO [dbo].[HackneyLicense]
-FROM 'https://azureopendatasets.blob.core.windows.net/nyctlc/yellow/puYear=2013/HackneyLicense'
-WITH
-(
-    FILE_TYPE = 'CSV',
-    FIELDTERMINATOR = ',',
-    FIELDQUOTE = ''
-);
+FROM 'https://azuresynapsestorage.blob.core.windows.net/nyctlc/hackney_license'
+WITH (FILE_TYPE = 'CSV', FIELDTERMINATOR = ',', FIELDQUOTE = '', FIRSTROW = 2);
 
 COPY INTO [dbo].[Medallion]
-FROM 'https://azureopendatasets.blob.core.windows.net/nyctlc/yellow/puYear=2013/Medallion'
-WITH
-(
-    FILE_TYPE = 'CSV',
-    FIELDTERMINATOR = ',',
-    FIELDQUOTE = ''
-);
+FROM 'https://azuresynapsestorage.blob.core.windows.net/nyctlc/medallion'
+WITH (FILE_TYPE = 'CSV', FIELDTERMINATOR = ',', FIELDQUOTE = '', FIRSTROW = 2);
 
 COPY INTO [dbo].[Time]
-FROM 'https://azureopendatasets.blob.core.windows.net/nyctlc/yellow/puYear=2013/Time'
-WITH
-(
-    FILE_TYPE = 'CSV',
-    FIELDTERMINATOR = ',',
-    FIELDQUOTE = ''
-);
+FROM 'https://azuresynapsestorage.blob.core.windows.net/nyctlc/time'
+WITH (FILE_TYPE = 'CSV', FIELDTERMINATOR = ',', FIELDQUOTE = '', FIRSTROW = 2);
 
 COPY INTO [dbo].[Weather]
-FROM 'https://azureopendatasets.blob.core.windows.net/nyctlc/yellow/puYear=2013/Weather'
-WITH
-(
-    FILE_TYPE = 'CSV',
-    FIELDTERMINATOR = ',',
-    FIELDQUOTE = '',
-    ROWTERMINATOR='0X0A'
-);
+FROM 'https://azuresynapsestorage.blob.core.windows.net/sampledata/nyctlc/weather'
+WITH (FILE_TYPE = 'CSV', FIELDTERMINATOR = ',', FIELDQUOTE = '', ROWTERMINATOR='0X0A', FIRSTROW = 2);
+
+-- ==========================================================
+-- 2. Trip Data (From Open Dataset Endpoint - Parquet)
+-- สำหรับปี 2013 ทั้งปี
+-- ==========================================================
 
 COPY INTO [dbo].[Trip]
-FROM 'https://azureopendatasets.blob.core.windows.net/nyctlc/yellow/puYear=2013/Trip2013'
-WITH
-(
-    FILE_TYPE = 'CSV',
-    FIELDTERMINATOR = '|',
-    FIELDQUOTE = '',
-    ROWTERMINATOR='0X0A',
-    COMPRESSION = 'GZIP'
-);
+FROM 'https://azureopendatastorage.blob.core.windows.net/nyctlc/yellow/puYear=2013/puMonth=*/*.parquet'
+WITH (FILE_TYPE = 'PARQUET');
